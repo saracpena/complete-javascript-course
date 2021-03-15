032939cdfaacfeba864ad0ec5d33f115ed52160f
Sara Peña <s.cristal.p@gmail.com>
2021-03-14 21:07:15 -0400

    Functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -33,7 +33,7 @@ const beefBurrito = taco('corn', 'beef');
 console.log(beefBurrito);
 
 
-//TODO: FUNCTION DECLARATION:
+//TODO: FUNCTION DECLARATION: can be used before it's declared
 function calcAge1(birthYear){
     return 2021 - birthYear;
 }
@@ -43,7 +43,7 @@ calcAge1(1985);
 const age = calcAge1(1985);
 console.log(age);
 
-//TODO: FUNCTION EXPRESSION/ANONYMOUS FUNCTION: a function without a name
+//TODO: FUNCTION EXPRESSION/ANONYMOUS FUNCTION: a function without a name, a function value stored in a variable
     // function (birthYear)
 //? completed:
     const calcAge2 = function(birthYear){
@@ -52,7 +52,7 @@ console.log(age);
  const age2 = calcAge2(2000);
  console.log(age2);
 
-//TODO: ARROW FUNCTION - shorter and faster to write ES6
+//TODO: ARROW FUNCTION - shorter and faster to write ES6, NO 'this' keyword
 const calcAge3 = birthYear => 2037 - birthYear;
 const age3 = calcAge3(2000);
 console.log(age3);
@@ -84,3 +84,37 @@ function fruitProcessor(apples, oranges){
 }
 
 console.log(fruitProcessor(2,3));
+
+//! REVIEW FUNCTIONS
+const calcAge = function (birthYear){
+    return 2037 - birthYear;
+}
+
+const yearsUntilRetirement1 = function (birthYear, firstName){
+    const age4 = calcAge(birthYear);
+    const retirement = 65 - age4;
+
+    if(retirement > 0){
+      console.log(`${firstName} retires in ${retirement} years`);
+      return retirement; 
+    } else {
+        console.log(`${firstName} has already retired!`);
+        return -1;
+    }
+
+    //return retirement; 
+    // return retirement;
+    // return `${firstName} retires in ${retirement} years`;
+}
+console.log(yearsUntilRetirement1(2000, 'Jonas'));
+console.log(yearsUntilRetirement1(1965, 'Buddy'));
+
+//! Try Exercise:
+
+function calcAge0(birthYear, firstName) {
+    const age1 = 2021 - birthYear;
+    console.log(`${firstName} is ${age1} years old`);
+    return age1;
+}
+
+const age1 = calcAge0(1985, 'Sara');
